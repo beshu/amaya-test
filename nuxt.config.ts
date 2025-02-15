4067d8aa2a0961819ba351e61e175a47956a0d88
Daniil Sheremet <beshu@github.com>
2025-02-16 01:20:26 +0300

    package json and nuxt config updated

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,22 @@
-export default defineNuxtConfig ({
+export default defineNuxtConfig({
   // Disable strict
   typescript: {
-    strict: false
+    typeCheck: true,
+    strict: false,
   },
 
   app: {
     head: {
-      title: 'Nuxt-Prisma Example (TypeScript)',
+      title: "Nuxt-Prisma Example (TypeScript)",
       meta: [
-        { charset: 'utf-8' },
-        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { hid: "description", name: "description", content: "" },
       ],
-      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    }
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
   },
 
-  compatibilityDate: '2025-02-15'
+  compatibilityDate: "2025-02-15",
 });
+
